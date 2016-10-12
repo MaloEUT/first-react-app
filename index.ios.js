f78d0dacfdf0000d94ce257c853358c312780085
MaloEUT <malo.rchrd@gmail.com>
2016-10-12 11:58:22 +0200

    first commit

---
+++ index.ios.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View,
+} from 'react-native';
+
+ 
+
+
+export default class nutApp extends Component {
+  render() {
+    return (
+      <View style={styles.container} >
+      <View style={styles.navbar}>
+      <View style={styles.navbarButton}></View>
+      <Text style={styles.navbarTitle}>NutApp</Text>
+      <Text style={styles.navbarButton}>search</Text>
+      </View>
+      <View style={styles.content}>
+      <Text >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+      </View>
+      </View>
+    );
+  }
+}
+
+
+ var styles = require('./styles')
+
+
+AppRegistry.registerComponent('nutApp', () => nutApp);
---
+++ styles.js
@@ -0,0 +1,46 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+
+
+export default StyleSheet.create({
+container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#DAE4E7',
+  },
+  navbar: {
+    backgroundColor: '#42476F',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row',
+  },
+  navbarTitle: {
+    backgroundColor: '#42476F',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    flex: 2,
+    color: '#DAE4E7'
+  },
+  navbarButton: {
+    backgroundColor: '#42476F',
+    width: 50,
+    flex: 0,
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
